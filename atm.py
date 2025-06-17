847bf973017ac82a2a02b78817df4b8b3e7243da
joyagergess <gergessjoya@gmail.com>
2025-06-17 10:32:13 +0300

    Add ATM menu with deposit and withdraw features

--- atm.py
+++ atm.py
@@ -1,4 +1,44 @@
 balance = 2000 
 deposit = 0
 withdraw = 0
+print ("welcome to ATM ")
+while True:
+    print("""
+         What can we help you with ?
+          1. Check Balance
+          2. Deposit Money
+          3. Withdraw Money
+          4. Exit
+          """)
+    choice = input ("Enter your choice (1 for checking balance , 2 for adding deposit etc..) :")
+    if choice =='1' :
+        print("Your current balance is :"+ str(balance))
+    elif choice== '2' :
+        while True:
+            deposit = input("Enter how much to deposit: ")
+            if not deposit.isdigit() :
+                 print("Please enter a valid positive number")
+            else :
+                deposit = int(deposit)
+                if deposit < 0:
+                   print("Enter a positive number")
+                else:
+                   balance += deposit
+                   print("Your current balance now is: " + str(balance))
+                   break
+                
+    elif choice =='3' :
+        while True:
+            withdraw = int(input("Enter how much to withdraw: "))
+            if withdraw > balance:
+                print("You don't have enough money. Try again.")
+            else:
+                balance = balance - withdraw
+                print("Withdrawal successful. New balance:", balance)
+                break   
+    elif choice == '4' :
+        print ("Exit ATM")
+        break
 
+    else :
+        print("Please enter a number between 1 and 4 ")
